57c1c95ba2428483d602cca63ef6e04022b666cf
Vladislav Vavilov <maksimsraud@gmail.com>
2024-07-03 23:43:35 +0300

    language options + refactoring

--- main.py
+++ main.py
@@ -15,55 +15,66 @@ def get_manga_title(manga):
 
 
 def select_manga():
+    '''Select language and exact title'''
     search_query = input("Enter manga's title: ")
-    manga_data = search_manga(search_query=search_query)
+    manga_data = search_manga(search_query)
 
-    manga_choices = list(map(get_manga_title, manga_data))
-    questions = [
-        inquirer.List('title', message='Select exact title',
-                      choices=manga_choices)
-    ]
+    title_choices = list(map(get_manga_title, manga_data))
+    title_questions = [inquirer.List(
+        'title', message='Select exact title', choices=title_choices)]
 
-    selected_manga_title = inquirer.prompt(questions)['title']
+    selected_manga_title = inquirer.prompt(title_questions)['title']
     selected_manga_data = list(filter(
         lambda manga: get_manga_title(
             manga) == selected_manga_title, manga_data
     ))[0]
 
-    return selected_manga_title, selected_manga_data['id']
+    language_choices = selected_manga_data['attributes']['availableTranslatedLanguages']
+    language_questions = [inquirer.List(
+        'language', message='Select language', choices=language_choices)]
+    selected_language = inquirer.prompt(language_questions)['language']
+
+    return selected_manga_title, selected_language, selected_manga_data['id']
 
 
 class MangaDownloader:
-    def __init__(self, title, id, step):
+    def __init__(self, title, language, id, step):
         self.title = title
+        self.language = language
         self.id = id
         self.step = step
 
     def get_chapters(self, limit=100, offset=0):
+        '''Request chunk of chapters'''
         chapters_request_params = {
             'limit': limit,
             'offset': offset,
-            'manga': self.id
+            'manga': self.id,
+            'translatedLanguage[]': self.language
         }
 
         chapters_response = requests.get(
             f'{BASE_URL}/chapter', params=chapters_request_params)
+
         return chapters_response.json()
 
     def get_chapter_path(self, chapter_title):
+        '''Get chapter's path in system'''
         if (not chapter_title):
             raise ValueError('Invalid chapter title')
 
         return os.path.join(os.getcwd(), 'manga', self.title, chapter_title)
 
-    def get_chapter_images_data(self, chapter_id):
+    def get_chapter_images(self, chapter_id):
+        '''Get chapter's images data'''
         chapter_images_response = requests.get(
             f'{BASE_URL}/at-home/server/{chapter_id}')
         return chapter_images_response.json()
 
     def download_chapter_images(self, chapter_id, chapter_path):
+        '''Request chapter's images and download it'''
         os.makedirs(chapter_path)
-        chapter_images_data = self.get_chapter_images_data(chapter_id)
+        chapter_images_data = self.get_chapter_images(chapter_id)
 
         for image in chapter_images_data['chapter']['data']:
             base_url = chapter_images_data['baseUrl']
@@ -74,15 +85,11 @@ class MangaDownloader:
             with open(f'{chapter_path}/{image}', 'wb') as file:
                 file.write(image_response.content)
 
-    def download_chapters(self, limit=100, offset=0):
-        chapters = self.get_chapters(limit, offset)['data']
-
-        for chapter in chapters:
+    def download_chapters(self, chapters):
+        '''Download all chapters'''
+        for i, chapter in enumerate(chapters):
             try:
-                if chapter['attributes']['translatedLanguage'] != 'en':
-                    continue
-
-                chapter_title = chapter['attributes']['title']
+                chapter_title = f'{i + 1}. {chapter['attributes']['title']}'
                 chapter_path = self.get_chapter_path(chapter_title)
                 is_chapter_exist = os.path.exists(chapter_path)
 
@@ -96,21 +103,25 @@ class MangaDownloader:
             except Exception as e:
                 print(f'Failed to retrieve a chapter: {e}')
 
-    def get_chapters_amount(self):
-        return self.get_chapters(limit=1, offset=0)['total']
-
     def download_manga(self):
-        total_chapters = self.get_chapters_amount()
+        '''Download all manga's chapters'''
+        chapters = self.get_chapters()
+        total_chapters = chapters['total']
+        print(f'Available {total_chapters} chapters in selected language')
 
+        '''Get all chapters by chunks'''
+        chapters_data = chapters['data']
         for i in range(0, total_chapters + self.step, self.step):
-            self.download_chapters(offset=i)
+            chapters_data.extend(self.get_chapters(offset=i)['data'])
+
+        self.download_chapters(chapters_data)
 
 
 def main():
     try:
-        manga_title, manga_id = select_manga()
+        title, language, id = select_manga()
 
-        downloader = MangaDownloader(title=manga_title, id=manga_id, step=100)
+        downloader = MangaDownloader(title, language, id, step=100)
         downloader.download_manga()
     except KeyboardInterrupt:
         print('\nDownload interrupted')
